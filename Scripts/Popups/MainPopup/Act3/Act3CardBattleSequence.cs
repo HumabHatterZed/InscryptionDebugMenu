7777ba742f9d290007560a36c87f8902430550d4
WhistleWind <humanhaterq@gmail.com>
2024-07-11 12:01:09 -0700

    P03 groundwork

--- Scripts/Popups/MainPopup/Act3/Act3CardBattleSequence.cs
+++ Scripts/Popups/MainPopup/Act3/Act3CardBattleSequence.cs
@@ -22,13 +22,12 @@ public class Act3CardBattleSequence : BaseCardBattleSequence
 		MapNodeManager mapNodeManager = Singleton<MapNodeManager>.m_Instance;
 		if (mapNodeManager == null)
 			return;
-
+        Window.Label($"{TurnManager.Instance.Opponent.GetType()}\nBlueprint: {TurnManager.Instance.Opponent.Blueprint.name}");
         MapNode nodeWithId = mapNodeManager.GetNodeWithId(RunState.Run.currentNodeId);
-
         if (nodeWithId?.Data is CardBattleNodeData cardBattleNodeData)
 		{
-            Window.Label($"Difficulty: {cardBattleNodeData.difficulty} + {RunState.Run.DifficultyModifier}");
-            Window.Label($"Turn Number: {TurnManager.Instance.TurnNumber}");
+            Window.Label($"Difficulty: {cardBattleNodeData.difficulty + RunState.Run.DifficultyModifier} ({cardBattleNodeData.difficulty} + {RunState.Run.DifficultyModifier})" +
+                $"\nTurn Number: {TurnManager.Instance.TurnNumber}");
         }
 
         base.OnGUI();
@@ -36,10 +35,12 @@ public class Act3CardBattleSequence : BaseCardBattleSequence
 
 	public override void AddBones(int amount)
 	{
-		
+		if (P03ModHelper.Enabled)
+			base.AddBones(amount);
 	}
 	public override void RemoveBones(int amount)
 	{
-		
-	}
+        if (P03ModHelper.Enabled)
+			base.RemoveBones(amount);
+    }
 }
---
+++ Scripts/Popups/MainPopup/Act3/P03ModHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using BepInEx.Bootstrap;
+
+namespace DebugMenu.Scripts.Act3;
+
+public static class P03ModHelper
+{
+    public static bool Enabled => Chainloader.PluginInfos.ContainsKey("zorro.inscryption.infiniscryption.p03kayceerun");
+    public static void PatchP03Mod() => Plugin.HarmonyInstance.PatchAll(typeof(P03ModHelper));
+
+    internal static bool addingMod = false;
+}
